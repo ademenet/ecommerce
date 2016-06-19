e7da6c8f80cbf077e00724d14df81e6feb1399fe
Antoine PAGET <apaget@e1r10p14.42.fr>
2016-06-19 19:35:48 +0200

    admin.php remove code inutile gestion produit

--- admin.php
+++ admin.php
@@ -25,42 +25,7 @@ if ($_SESSION['userinfo']['admin'] !== "") {
 		}
 	}
 	if (isset($_GET['action'])) {
-		if ($_GET['action'] == 'add') {
-			if (isset($_POST['submit'])) {
-				if ($_POST['title'] && $_POST['desc'] && $_POST['price']) {
-					add_product($_POST['title'], $_POST['price'], $_POST['desc'],0,0,0,1,"../zbri","foot", 42);
-				} else {
-					echo "PAS OK\n";
-				}
-			}
- ?>
-
-<form action="" method="POST">
-	<p>Nom du produit : <input type="text" name="title" value=""></p><br />
-	<p>Description : <input type="text" name="desc" value=""></p><br />
-	<p>Prix : <input type="text" name="price" value=""></p>
-	<input type="submit" name="Ajouter" value="OK">
-</form>
-
-<?php
-		}
-		elseif ($_GET['action'] == modify) {
-
-		}
-		elseif ($_GET['action'] == delete) {
-			if ($_POST['delete'] == "OK" && isset($_POST['deleted_game']))
-			{
-				rm_product($_POST['deleted_game']);
-			}
-
-?>
-	<form action="" method="post">
-		<p>Nom du produit a supprimer : <input type="text" name="deleted_game" value="Barbie land" /></p>
-		<input type="submit" name="delete" value="OK" />
-	</form>
-<?php
-		}
-		elseif ($_GET['action'] == addusr) {
+		if ($_GET['action'] == addusr) {
 			include('./includes/inscription-form.php');
 		} elseif ($_GET['action'] == modifyusr || $_GET['action'] == deleteusr) {
 			display_users_for_admin_modify_delete($_SESSION['userinfo']['login']);
