216a36872c0bfd71455cbff8e217df8a28668300
Alain DEMENET <ademenet@student.42.fr>
2016-06-19 11:16:55 +0200

    up

--- inscription.php
+++ inscription.php
@@ -10,7 +10,7 @@ if ($_POST['submit'] === "Valider") {
 			alert("Le format de votre mail n'est pas valide");
 		} else {
 			if (check_user(secu($_POST['usrlog']))) {
-				add_user(secu($_POST['usrlog']), secu($_POST['usrpwd']), secu($_POST['usrprenom']), secu($_POST['usrnom']), secu($_POST['usrtel']), secu($_POST['usrmail')], secu($_POST['usraddress']));
+				add_user(secu($_POST['usrlog']), secu($_POST['usrpwd']), secu($_POST['usrprenom']), secu($_POST['usrnom']), secu($_POST['usrtel']), secu($_POST['usrmail']), secu($_POST['usraddress']));
 				$_SESSION['userinfo'] = get_userinfos(secu($_POST['usrlog']));
 				valid("Bienvenue ".$_SESSION['userinfo']['login']." sur &#10084 games");
 				echo "<script>setTimeout(\"document.location.href = 'index.php';\",2000);</script>";
